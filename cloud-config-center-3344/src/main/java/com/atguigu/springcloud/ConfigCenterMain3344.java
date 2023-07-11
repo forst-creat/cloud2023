af8c7332b377cdb6603d8e2f2f40e02cf2407788
liuyang <1459410546@qq.com>
2023-07-12 01:58:36 +0800

    23/07/12 cloud分布式配置中心实现服务端配置，存在问题：SSH类型uri无法访问

---
+++ cloud-config-center-3344/src/main/java/com/atguigu/springcloud/ConfigCenterMain3344.java
@@ -0,0 +1,22 @@
+package com.atguigu.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * ClassName: ConfigCenterMain3344
+ * Description:
+ *
+ * @Author: liuyang
+ * @Create: 2023/7/11 - 16:28
+ */
+@SpringBootApplication
+@EnableEurekaClient
+@EnableConfigServer
+public class ConfigCenterMain3344 {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigCenterMain3344.class, args);
+    }
+}
