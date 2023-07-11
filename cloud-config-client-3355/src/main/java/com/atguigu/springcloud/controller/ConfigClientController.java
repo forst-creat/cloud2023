6943bd8fa31cca3a659647efd59abba13de7fa95
liuyang <1459410546@qq.com>
2023-07-12 03:52:06 +0800

    23/07/12 cloud配置中心实现客户端配置动态刷新

--- cloud-config-client-3355/src/main/java/com/atguigu/springcloud/controller/ConfigClientController.java
+++ cloud-config-client-3355/src/main/java/com/atguigu/springcloud/controller/ConfigClientController.java
@@ -2,6 +2,7 @@ package com.atguigu.springcloud.controller;
 
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
  * @Create: 2023/7/12 - 2:29
  */
 @RestController
+@RefreshScope
 public class ConfigClientController {
 
     @Value("${config.info}")
