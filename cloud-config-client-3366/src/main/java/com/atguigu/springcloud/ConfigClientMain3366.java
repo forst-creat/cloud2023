3f0381d532c11e3e23d8230fc127d7848050dfd3
liuyang <1459410546@qq.com>
2023-07-13 00:55:30 +0800

    23/07/13 cloud配置中心实现用bus完成动态全局广播刷新及定点通知

---
+++ cloud-config-client-3366/src/main/java/com/atguigu/springcloud/ConfigClientMain3366.java
@@ -0,0 +1,20 @@
+package com.atguigu.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * ClassName: ConfigClientMain3366
+ * Description:
+ *
+ * @Author: liuyang
+ * @Create: 2023/7/12 - 20:38
+ */
+@SpringBootApplication
+@EnableEurekaClient
+public class ConfigClientMain3366 {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigClientMain3366.class, args);
+    }
+}
---
+++ cloud-config-client-3366/src/main/java/com/atguigu/springcloud/controller/ConfigClientController.java
@@ -0,0 +1,26 @@
+package com.atguigu.springcloud.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * ClassName: ConfigClientController
+ * Description:
+ *
+ * @Author: liuyang
+ * @Create: 2023/7/12 - 20:41
+ */
+@RestController
+public class ConfigClientController {
+    @Value("${server.port}")
+    private String serverPort;
+
+    @Value("${config.info}")
+    private String configInfo;
+
+    @GetMapping("/configInfo")
+    public String configInfo() {
+        return "serverPort: " + serverPort + "\t\n\n configInfo: " + configInfo;
+    }
+}
