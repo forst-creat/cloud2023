8d6261b7b929554a1aa89ac79afc5f1646ce826a
liuyang <1459410546@qq.com>
2023-07-13 20:59:27 +0800

    23/07/13 cloud中sleuth链路跟踪实现消费者到生产者之间消息传递面板

--- cloud-consumer-order80/src/main/java/com/atguigu/springcloud/controller/OrderController.java
+++ cloud-consumer-order80/src/main/java/com/atguigu/springcloud/controller/OrderController.java
@@ -44,6 +44,7 @@ public class OrderController {
 
     /**
      * 测试手写轮询算法
+     *
      * @param id
      * @return
      */
@@ -78,6 +79,7 @@ public class OrderController {
 
     /**
      * 手写轮询算法实现
+     *
      * @return
      */
     @GetMapping("/consumer/payment/lb")
@@ -92,4 +94,18 @@ public class OrderController {
 
         return restTemplate.getForObject(uri + "/payment/lb", String.class);
     }
+
+
+    /**
+     * zipkin+sleuth
+     *
+     * @return
+     */
+    @GetMapping("/consumer/payment/zipkin")
+    public String paymentZipkin() {
+        String result = restTemplate.getForObject("http://localhost:8001" + "/payment/zipkin/", String.class);
+        return result;
+    }
+
+
 }
--- cloud-provider-payment8001/src/main/java/com/atguigu/springcloud/controller/PaymentController.java
+++ cloud-provider-payment8001/src/main/java/com/atguigu/springcloud/controller/PaymentController.java
@@ -92,4 +92,12 @@ public class PaymentController {
         return serverPort;
     }
 
+    /**
+     * 链路zipkin
+     * @return
+     */
+    @GetMapping("/payment/zipkin")
+    public String paymentZipkin() {
+        return "hi ,i'am paymentzipkin server fall back，welcome to atguigu，O(∩_∩)O哈哈~";
+    }
 }
