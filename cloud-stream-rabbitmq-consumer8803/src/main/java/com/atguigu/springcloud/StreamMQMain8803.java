644220f3ff5daa392fc9c4e3cde802af586633ea
liuyang <1459410546@qq.com>
2023-07-13 15:41:47 +0800

    23/07/13 cloud中Stream消息驱动yml配置group对消费者进行分组解决重复消费及消息持久化问题

---
+++ cloud-stream-rabbitmq-consumer8803/src/main/java/com/atguigu/springcloud/StreamMQMain8803.java
@@ -0,0 +1,20 @@
+package com.atguigu.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * ClassName: StreamMQMain8803
+ * Description:
+ *
+ * @Author: liuyang
+ * @Create: 2023/7/13 - 15:27
+ */
+@SpringBootApplication
+@EnableEurekaClient
+public class StreamMQMain8803 {
+    public static void main(String[] args) {
+        SpringApplication.run(StreamMQMain8803.class, args);
+    }
+}
